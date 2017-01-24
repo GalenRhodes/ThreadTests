edfeafa83721d0053b1fc4ccc03265fdabd6a2d9
Galen Rhodes <galenrhodes@me.com>
2017-01-24 06:06:57 -0500

    Initial Commit

---
+++ ThreadTests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreadTests
+//
+//  Created by Galen Rhodes on 1/24/17.
+//  Copyright © 2017 Project Galen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
